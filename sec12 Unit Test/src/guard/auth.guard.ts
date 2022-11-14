354eb4a7beced249676fc66eed925b5fd8b960dc
SOHTA0318 <nicosohta0318@gmail.com>
2022-11-14 21:36:43 +0900

    unit test

---
+++ sec12 Unit Test/src/guard/auth.guard.ts
@@ -0,0 +1,9 @@
+/* eslint-disable prettier/prettier */
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+@Injectable()
+export class AuthGuard implements CanActivate {
+  canActivate(context: ExecutionContext) {
+    const request = context.switchToHttp().getRequest();
+    return request.session.userId;
+  }
+}
---
+++ sec12 Unit Test/src/reports/reports.controller.ts
@@ -0,0 +1,42 @@
+import {
+  Body,
+  Controller,
+  Post,
+  UseGuards,
+  Patch,
+  Param,
+  Get,
+  Query,
+} from '@nestjs/common';
+import { CreateReportDto } from './dtos/create-report.dto';
+import { ReportsService } from './reports.service';
+import { AuthGuard } from '../guard/auth.guard';
+import { CurrentUser } from 'src/users/decorators/current-user.decorator';
+import { User } from 'src/users/user.entity';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { ReportDto } from './dtos/report.dto';
+import { ApproveReportDto } from './dtos/approve-report.dto';
+import { AdminGuard } from 'src/guard/admin.guard';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
+
+@Controller('reports')
+export class ReportsController {
+  constructor(private reportService: ReportsService) {}
+  @Post()
+  @UseGuards(AuthGuard)
+  @Serialize(ReportDto)
+  createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
+    return this.reportService.create(body, user);
+  }
+
+  @Patch('/:id')
+  @UseGuards(AdminGuard)
+  approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
+    return this.reportService.changeApproval(parseInt(id), body.approved);
+  }
+
+  @Get()
+  getEstimate(@Query() query: GetEstimateDto) {
+    return this.reportService.createEstimate(query);
+  }
+}
